63107cf9c24435d6159862f10a7b5bca2c392c32
Pradeep-003 <pradeep50953@gmail.com>
2025-08-29 16:22:06 +0530

    url

--- client/src/api/axios.jsx
+++ client/src/api/axios.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 API.interceptors.request.use((req) => {
