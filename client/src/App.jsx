7b3dd17c6cdd1d39bb16e4521446978ddd25da6e
Pradeep-003 <pradeep50953@gmail.com>
2025-08-29 17:49:36 +0530

    hii

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -19,7 +19,7 @@ function AppRoutes() {
   const { token, role } = useContext(AuthContext);
 
   return (
-    <Routes>
+    <Routes basename="/roi-mern-app">
       {/* Public */}
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
@@ -129,7 +129,7 @@ function AppRoutes() {
 function App() {
   return (
     <AuthProvider>
-      <BrowserRouter basename="/roi-mern-app">
+      <BrowserRouter>
         <AppRoutes />
       </BrowserRouter>
     </AuthProvider>
